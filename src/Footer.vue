b0f1415b908e6e84ab40d21ee671d4dac716057f
Evan Cestnick <evan@cestnick.com>
2023-05-13 22:47:33 -0300

    Added GitHub link/icon

--- src/Footer.vue
+++ src/Footer.vue
@@ -1,7 +1,12 @@
 <template>
   <footer class="footer mt-auto py-3 bg-light">
-    <div class="container">
-      <span class="text-muted">Created by Evan Cestnick</span>
+    <div class="d-flex justify-content-between container">
+      <div>
+        <span class="text-muted ">Created by Evan Cestnick</span>
+      </div>
+      <div>
+        <a href="https://github.com/NotoriS/tube-stamp" target="_blank" class="text-reset"><i class="logo fa fa-lg fa-github"></i></a>
+      </div>
     </div>
   </footer>
 </template>
