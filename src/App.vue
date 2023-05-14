0fc60238fddb9c54ca4a2acb5b3154708cd80e1e
Evan Cestnick <evan@cestnick.com>
2023-05-14 00:14:52 -0300

    Hooked up form input

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,17 @@ export default {
   components: {
     Header,
     Footer
+  },
+  data() {
+    return {
+      URL: ''
+    }
+  },
+  methods: {
+    getVideoData() {
+      // Get video data
+      this.URL = ''
+    }
   }
 }
 </script>
@@ -15,11 +26,12 @@ export default {
   <main>
     <div class="container">
       <div class=" container bg-light mt-5 p-5 border">
-        <h2 class="text-center mb-4">Find a YouTube Upload Date</h2>
-        <div class="d-flex flex-row mx-auto">
-          <input type="text" class="form-control" style="margin-right: 10px" placeholder="Enter a YouTube URL">
-          <button class="btn btn-dark">Enter</button>
-        </div>
+        <h2 class="text-muted text-center mb-4">Find a YouTube Upload Date</h2>
+        <form class="d-flex flex-row mx-auto" onsubmit="return false">
+          <input v-model="URL" type="text" class="form-control" style="margin-right: 10px" placeholder="Enter a YouTube URL">
+          <button @click="getVideoData" type="submit" class="btn btn-dark">Enter</button>
+        </form>
+        <p>{{URL}}</p>
       </div>
     </div>
   </main>
