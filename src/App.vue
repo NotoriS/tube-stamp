dd425441af89e1f3f258507cda7807fd426d0129
Evan Cestnick <evan@cestnick.com>
2023-05-13 21:48:22 -0300

    Added header

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
 </script>
 
 <template>
+  <header class="header py-3 bg-dark">
+    <div class="container">
+      <h1>
+        <span class="logo-1">Tube</span>
+        <span class="logo-2">Stamp</span>
+      </h1>
+    </div>
+  </header>
   <main>
     <div class="container">
       <h2>Find the exact date and time any YouTube video was uploaded</h2>
@@ -17,3 +25,12 @@
     </div>
   </footer>
 </template>
+
+<style type="text/css">
+.logo-1 {
+  color: white;
+}
+.logo-2 {
+  color: red;
+}
+</style>
