fc34cba495afa997ce896beefc4725b35138bce2
Evan Cestnick <evan@cestnick.com>
2023-05-13 21:37:34 -0300

    Added Bootstrap v5.0

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,18 @@
 </script>
 
 <template>
-  <h2>Find the exact date and time any YouTube video was uploaded</h2>
-  <form>
-  	<input placeholder="Enter a YouTube URL">
-  	<button>Enter</button>
-  </form>
+  <main>
+    <div class="container">
+      <h2>Find the exact date and time any YouTube video was uploaded</h2>
+      <form>
+        <input placeholder="Enter a YouTube URL">
+        <button>Enter</button>
+      </form> 
+    </div>
+  </main>
+  <footer class="footer mt-auto py-3 bg-light">
+    <div class="container">
+      <span class="text-muted">Created by Evan Cestnick</span>
+    </div>
+  </footer>
 </template>
