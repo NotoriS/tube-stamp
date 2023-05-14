5018171180b6ac0f15aefea843c5d6266e810f41
Evan Cestnick <evan@cestnick.com>
2023-05-13 21:09:05 -0300

    Added HTML content to the body

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
-<script setup>
-
+<script>
 </script>
 
 <template>
-  
+  <h2>Find the exact date and time any YouTube video was uploaded</h2>
+  <form>
+  	<input placeholder="Enter a YouTube URL">
+  	<button>Enter</button>
+  </form>
 </template>
-
-<style scoped>
-
-</style>
