579c18427a8a1f74dde11a8afbb6e10f56d2519b
Evan Cestnick <evan@cestnick.com>
2023-05-14 22:38:40 -0300

    Moved getVideoData call to a form attribute

--- src/App.vue
+++ src/App.vue
@@ -27,9 +27,9 @@ export default {
     <div class="container">
       <div class=" container bg-light mt-5 p-5 border">
         <h2 class="text-muted text-center mb-4">Find a YouTube Upload Date</h2>
-        <form class="d-flex flex-row mx-auto" onsubmit="return false">
+        <form class="d-flex flex-row mx-auto" @submit.prevent="getVideoData">
           <input v-model="URL" type="text" class="form-control" style="margin-right: 10px" placeholder="Enter a YouTube URL">
-          <button @click="getVideoData" type="submit" class="btn btn-dark">Enter</button>
+          <button type="submit" class="btn btn-dark">Enter</button>
         </form>
         <p>{{URL}}</p>
       </div>
