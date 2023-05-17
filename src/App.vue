8ec00831ad76bf9a645600831d57a0fd2a9f957a
Evan Cestnick <evan@cestnick.com>
2023-05-16 22:28:44 -0300

    Removed shadow from form elements

--- src/App.vue
+++ src/App.vue
@@ -48,8 +48,8 @@ export default {
       <div class=" container bg-light mt-5 p-5 border">
         <h2 class="text-muted text-center mb-4">Find a YouTube Upload Date</h2>
         <form class="d-flex flex-row mx-auto" @submit.prevent="getVideoData">
-          <input v-model="URL" type="text" class="form-control input-margin" :class="inputValidity" placeholder="Enter a YouTube URL">
-          <button type="submit" class="btn btn-dark">Enter</button>
+          <input v-model="URL" type="text" class="form-control input-margin shadow-none" :class="inputValidity" placeholder="Enter a YouTube URL">
+          <button type="submit" class="btn btn-dark shadow-none">Enter</button>
         </form>
         <div v-if="inputValidity == 'is-invalid'" class="warning-block p-3 mt-5">
           <p class="text-center m-auto">The URL you have entered is invalid.</p>
